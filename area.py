972d9473d4d9974dd1687a06d0627c92f098f3c8
RashiSah <rashi.747@gmail.com>
2018-05-31 01:29:04 +0530

    area table

---
+++ area.py
@@ -0,0 +1,30 @@
+from brainzutils import musicbrainz_db
+from sqlalchemy import text
+
+with musicbrainz_db.engine.begin() as connection:
+ 	query = text("""
+ 		SELECT area.id,
+ 			   area.gid,
+ 			   area.name,
+ 			   area.type,
+ 			   area.edits_pending,
+ 			   area.last_updated,
+ 			   area.begin_date_year,
+ 			   area.begin_date_month,
+ 			   area.begin_date_day,
+			   area.end_date_year,
+			   area.end_date_month,
+			   area.end_date_day,
+			   area.ended, 
+			   area.comment
+		  FROM area
+	INNER JOIN artist
+			ON area.id = artist.area
+	INNER JOIN artist_credit
+			ON artist.id = artist_credit.id
+		 WHERE artist_credit.id = :MB_artist_data[0]
+	"""),{
+ 		"MB_artist_data": MB_artist_data,
+	}
+ 	result = connection.execute(query)
+ 	MB_area_data = result.fetchone()
